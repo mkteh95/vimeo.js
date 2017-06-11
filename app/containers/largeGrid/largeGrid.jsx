8870c3e80d20652bb9928ed1a658be571ad95d67
Teh Mun Kit <mkteh95@hotmail.com>
2017-06-11 01:21:33 -0700

    Added message when response is empty

--- app/containers/largeGrid/largeGrid.jsx
+++ app/containers/largeGrid/largeGrid.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
+import ExceptionPage from '../../pages/exception/exception.jsx'
+
 import style from './style.scss'
 
 
 class LargeGrid extends React.Component {
 
   render() {
-    return (
-      <div className={style.largeGrid}>
-        {this.props.children}
-      </div>
-    )
+    return (Array.isArray(this.props.children))
+      ? (this.props.children.length > 0)
+        ? (
+            <div className={style.largeGrid}>
+              {this.props.children}
+            </div>
+          )
+        : (
+             <ExceptionPage title="No content available." />
+          )
+      : null
   }
 
 }
--- app/containers/mediumGrid/mediumGrid.jsx
+++ app/containers/mediumGrid/mediumGrid.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
+import ExceptionPage from '../../pages/exception/exception.jsx'
+
 import style from './style.scss'
 
 
 class MediumGrid extends React.Component {
 
   render() {
-    return (
-      <div className={style.mediumGrid}>
-        {this.props.children}
-      </div>
-    )
+    return (Array.isArray(this.props.children))
+      ? (this.props.children.length > 0)
+        ? (
+            <div className={style.mediumGrid}>
+              {this.props.children}
+            </div>
+          )
+        : (
+             <ExceptionPage title="No content available." />
+          )
+      : null
   }
 
 }
