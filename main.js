6497a6bb6e9a350795ccaa2b9d6b7387563d0b67
Teh Mun Kit <mkteh95@hotmail.com>
2017-06-11 17:45:14 -0700

    Removed Vimeo JS SDK and changed title in titlebar

--- main.js
+++ main.js
@@ -2,13 +2,9 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-var Vimeo = require('vimeo').Vimeo
-var lib = new Vimeo('9bb54648addc57804fc4bca391a84c12a7ed8e8d', 
-                        'I0D5JrHsNdBk0xdyu9zXG/Y3TVdd8/Z8IijOWqfQ7jupIy5zgzthWsFhLFRkr1igkX4T2/ZpXcbYGU4Tu294dxXOYk45hTLISRKN+ysb9OmE1fcDWTbISShsBr+B22BR')
-
-
 app.on('ready', () => {
   const screen = electron.screen.getPrimaryDisplay()
+  
   mainWindow = new BrowserWindow({
     height: screen.bounds.height,
     width: screen.bounds.width,
