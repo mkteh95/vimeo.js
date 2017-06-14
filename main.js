7accc4726c98e0db42585b4512227f4fa88e9501
Teh Mun Kit <mkteh95@hotmail.com>
2017-06-13 17:45:49 -0700

    Added hide on close for OSX

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const electron = require('electron')
 const app = electron.app
+const os = require("os");
 const BrowserWindow = electron.BrowserWindow
+
+let quit = false
 
 app.on('ready', () => {
   const screen = electron.screen.getPrimaryDisplay()
@@ -17,5 +20,22 @@ app.on('ready', () => {
     pathname: require('path').join(__dirname, 'index.html')
   })
 
+  window.on('close', (e) => {
+    if (os.plarform === 'darwin' && !quit) {
+      e.preventDefault()
+      mainWindow.hide()
+    } else {
+      mainWindow = null
+    }
+  })
+
   mainWindow.loadURL(url)
+})
+
+app.on('activate', () => {
+  mainWindow.show()
+})
+
+app.on('before-quit', () => {
+  quit = true
 })
