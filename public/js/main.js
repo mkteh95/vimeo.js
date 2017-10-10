ed46bc8b75cf8e6cc829b31964d2cd85708cd7e1
Teh Mun Kit <mkteh95@hotmail.com>
2017-10-09 22:47:58 -0700

    Fixed navbar links not navigating.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('body').scrollspy({
-      offset: $('#navbar').height()
+      offset: $('#navbar').height() + 1
   })
 
   $('.navbar-nav li:not(.icon) a, .navbar-brand').on('click', function(e) {
       e.preventDefault();
-      $('body').animate({
+      $('html, body').animate({
           scrollTop: $(this.hash).offset().top - $('#navbar').height()
       }, 300);
   });
